3a84b3f7996a91365363b33ccf950e03f1a6e61c
pulipatirohithkumar74-wq <pulipatirohithkumar74@gmail.com>
2026-02-06 12:08:14 +0530

    Create Quotient and Remainder.c

---
+++ Quotient and Remainder.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main() {
+    //Your Code goes here!
+    int a,b;
+    scanf("%d%d",&a,&b);
+    printf("Quotient=%d\n",a/b);
+    printf("remainder=%d",a%b);
+    return 0;
+}
