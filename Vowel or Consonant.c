fdd2a33f32c9cb3f987cb01650809ba08eae4918
pulipatirohithkumar74-wq <pulipatirohithkumar74@gmail.com>
2026-02-09 11:46:42 +0530

    Create Vowel or Consonant.c

---
+++ Vowel or Consonant.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+    //Your Code goes here!
+    int a;
+    scanf("%c",&a);
+    if(a=='a'||a=='e'||a=='i'||a=='o'||a=='u'||a=='A'||a=='E'||a=='I'||a=='O'||a=='U'){
+        printf("vowel");
+    }
+    else {
+        printf("consonent");
+    }
+    return 0;
+}
