918111ee64eeab2f450dba2eff3b0cc1f12b3cff
pulipatirohithkumar74-wq <pulipatirohithkumar74@gmail.com>
2026-02-09 14:46:15 +0530

    Create Electricity Bill.c

---
+++ Electricity Bill.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main() {
+    //Your Code goes here!
+    int a,b;
+    scanf("%d",&a,&b);
+    if(a<=100){
+        b=a*5;
+    }
+    else{
+        b=a*5;
+    }
+    printf("b=Rs.%d",b);
+    return 0;
+}
