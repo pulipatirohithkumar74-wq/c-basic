02ad8b294a6aa3f093e26facd4aa87d62a2090df
pulipatirohithkumar74-wq <pulipatirohithkumar74@gmail.com>
2026-02-09 11:44:25 +0530

    Create Check Divisible by 5.c

---
+++ Check Divisible by 5.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+    //Your Code goes here!
+    int a;
+    scanf("%d",&a);
+    if(a%5==0){
+        printf("Divisible by 5");
+    }
+    else{
+        printf("not divisible by 5");
+    }
+    return 0;
+}
