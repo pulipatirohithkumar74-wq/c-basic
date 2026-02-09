5ca0b4c48343ce77d8bd2777efbac548278d51c2
pulipatirohithkumar74-wq <pulipatirohithkumar74@gmail.com>
2026-02-09 11:49:42 +0530

    Create Check 3-Digit Number.c

---
+++ Check 3-Digit Number.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+    //Your Code goes here!
+    int a;
+    scanf("%d",&a);
+    if(a>=100&&a<=999){
+        printf("three-digit number");
+    }
+    else{
+        printf("not three digit number");
+    }
+    return 0;
+}
