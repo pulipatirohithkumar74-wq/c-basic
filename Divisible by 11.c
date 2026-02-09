d4af99ed6621f15e318cfba84fa394c2f312e1c8
pulipatirohithkumar74-wq <pulipatirohithkumar74@gmail.com>
2026-02-09 14:20:49 +0530

    Create Divisible by 11.c

---
+++ Divisible by 11.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+    //Your Code goes here!
+     int a;
+    scanf("%d",&a);
+    if(a%11==0){
+        printf("Divisible by 11");
+    }
+    else{
+        printf("not divisible by 11");
+    }
+    return 0;
+}
