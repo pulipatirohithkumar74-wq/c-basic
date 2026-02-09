923ba0e5467a432956db59af737819c08a29bd9c
pulipatirohithkumar74-wq <pulipatirohithkumar74@gmail.com>
2026-02-09 11:48:57 +0530

    Create Voting Eligibility.c

---
+++ Voting Eligibility.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+    //Your Code goes here!
+    int a;
+    scanf("%d",&a);
+    if(a>=18){
+        printf("Eligible for vote");
+    }
+    else{
+        printf("not eligible for vote");
+    }
+    return 0;
+}
