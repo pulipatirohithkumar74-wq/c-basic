0c6fb72ac21290425edd89b20f37d0bd1e267e13
pulipatirohithkumar74-wq <pulipatirohithkumar74@gmail.com>
2026-02-10 10:39:38 +0530

    Create  Internet Data Usage Alert.c

---
+++ Internet Data Usage Alert.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+    //Your Code goes here!
+    float a;
+    scanf("%f",&a);
+    if(a>2){
+        printf("data limit exeeded");
+    }
+    else{
+        printf("data within limit");
+    }
+    return 0;
+}
