532d7fe66bf478e89d766f47a1604963e1511efe
pulipatirohithkumar74-wq <pulipatirohithkumar74@gmail.com>
2026-02-10 10:38:05 +0530

    Create Online Order Discount.c

---
+++ Online Order Discount.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+    //Your Code goes here!
+    int a;
+    scanf("%d",&a);
+    if(a>=1000){
+        printf("discount applied");
+    }
+    else{
+        printf("no discount");
+    }
+    return 0;
+}
