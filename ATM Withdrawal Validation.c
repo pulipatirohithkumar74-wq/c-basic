623a601530519fbf304a2b567826b222e3b059fb
pulipatirohithkumar74-wq <pulipatirohithkumar74@gmail.com>
2026-02-10 10:40:16 +0530

    Implement ATM withdrawal amount validation

---
+++ ATM Withdrawal Validation.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+    //Your Code goes here!
+    int a;
+    scanf("%d",&a);
+    if(a%100==0){
+        printf("valid Amount");
+    }
+    else{
+        printf("Invalid amount");
+    }
+    return 0;
+}
