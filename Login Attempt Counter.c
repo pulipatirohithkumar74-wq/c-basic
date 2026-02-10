af2753bf9f2fcb11db7bef156c4fe754cc7d3c28
pulipatirohithkumar74-wq <pulipatirohithkumar74@gmail.com>
2026-02-10 10:45:05 +0530

    Implement login attempt counter with lockout

---
+++ Login Attempt Counter.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+    //Your Code goes here!
+    int a;
+    scanf("%d",&a);
+    if(a>3){
+        printf("Account Locked");
+    }
+    else{
+        printf("Login Allowed");
+    }
+    return 0;
+}
