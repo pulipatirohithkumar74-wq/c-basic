313ca0ceda2e1f4fc8d671ec618994fe40838392
pulipatirohithkumar74-wq <pulipatirohithkumar74@gmail.com>
2026-02-10 10:42:59 +0530

    Create Vehicle Speed Limit.c

---
+++ Vehicle Speed Limit.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+    //Your Code goes here!
+    int a;
+    scanf("%d",&a);
+    if(a>80){
+        printf("Speed limit exeeded");
+    }
+    else{
+        printf("Within the speed limit");
+    }
+    return 0;
+}
