449b50eb4f639f97178bb83e779b08a3a07af7fd
pulipatirohithkumar74-wq <pulipatirohithkumar74@gmail.com>
2026-02-10 10:37:30 +0530

    Create  Password Length Check.c

---
+++ Password Length Check.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+    //Your Code goes here!
+    int a;
+    scanf("%d",&a);
+    if(a>=8){
+        printf("strong password");
+    }
+    else{
+        printf("weak password");
+    }
+    return 0;
+}
