e9272dfb7739570e42ac72b2dece500eaa4a1d9a
pulipatirohithkumar74-wq <pulipatirohithkumar74@gmail.com>
2026-02-10 10:36:42 +0530

    Create Salary Bonus Eligibility.c

---
+++ Salary Bonus Eligibility.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+    //Your Code goes here!
+    int a;
+    scanf("%d",&a);
+    if(a>30000){
+        printf("eligible for bonus");
+    }
+    else{
+        printf("not eligible for bonus");
+    }
+    return 0;
+}
