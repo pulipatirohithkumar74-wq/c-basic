0a9e6c443d46e94dbd9f2146c236eaaa90435a14
pulipatirohithkumar74-wq <pulipatirohithkumar74@gmail.com>
2026-02-10 10:42:06 +0530

    Create Weekend Checker.c

---
+++ Weekend Checker.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+    //Your Code goes here!
+    int a;
+    scanf("%d",&a);
+    if(a==6||a==7){
+        printf("Weekend");
+    }
+    else{
+        printf("Weekday");
+    }
+    return 0;
+}
