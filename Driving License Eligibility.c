6fb5bef57198fc7c0cce816ce9a3aa0ebf16da84
pulipatirohithkumar74-wq <pulipatirohithkumar74@gmail.com>
2026-02-10 10:45:45 +0530

    Implement driving license eligibility check

---
+++ Driving License Eligibility.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+    //Your Code goes here!
+    int a;
+    scanf("%d",&a);
+    if(a>=18&&a<=70){
+        printf("Eligible");
+    }
+    else{
+        printf("Not Eligible");
+    }
+    return 0;
+}
