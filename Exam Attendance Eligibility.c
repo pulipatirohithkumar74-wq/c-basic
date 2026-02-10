6bb43351d87e9ba35e94c0394e8260da8da0868f
pulipatirohithkumar74-wq <pulipatirohithkumar74@gmail.com>
2026-02-10 10:47:12 +0530

    Add eligibility check for exam attendance

---
+++ Exam Attendance Eligibility.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+    //Your Code goes here!
+    int a;
+    scanf("%d",&a);
+    if(a>=75){
+        printf("Eigible for exam");
+    }
+    else{
+        printf("Not Eligible ");
+    }
+    return 0;
+}
