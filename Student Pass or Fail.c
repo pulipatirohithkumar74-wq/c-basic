10eb967aef5037d4398907ba597a240300a76e98
pulipatirohithkumar74-wq <pulipatirohithkumar74@gmail.com>
2026-02-10 10:41:24 +0530

    Create  Student Pass or Fail.c

---
+++ Student Pass or Fail.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+    //Your Code goes here!
+    int a;
+    scanf("%d",&a);
+    if(a>=40){
+        printf("PASS");
+    }
+    else{
+        printf("FAIL");
+    }
+    return 0;
+}
