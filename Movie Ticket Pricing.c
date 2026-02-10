6007e6cd44d893ae3a7e437990a87777171488c1
pulipatirohithkumar74-wq <pulipatirohithkumar74@gmail.com>
2026-02-10 10:46:24 +0530

    Implement ticket pricing based on age

---
+++ Movie Ticket Pricing.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+    //Your Code goes here!
+    int a;
+    scanf("%d",&a);
+    if(a<12){
+        printf("Discount Ticket");
+    }
+    else{
+        printf("Regular Ticket");
+    }
+    return 0;
+}
