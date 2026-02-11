924acac407b632ab438b9075e9e347d5f10d33d1
pulipatirohithkumar74-wq <pulipatirohithkumar74@gmail.com>
2026-02-11 15:20:12 +0530

    Add Even Numbers Printer program in C

---
+++ Even Numbers Printer.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+    //Your Code goes here!
+    int a,i;
+    scanf("%d",&a);
+    for(i=0;i<=a;i+=2){
+        
+        printf("%d\n",i);
+    }
+    return 0;
+}
