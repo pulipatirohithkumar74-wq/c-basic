c16d3dafd3b12a635a8bb3b8499fae377aaaf6f1
pulipatirohithkumar74-wq <pulipatirohithkumar74@gmail.com>
2026-02-11 15:27:49 +0530

    Add multiplication table program
    Implement multiplication table for a given number.

---
+++ Multiplication Table.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+    //Your Code goes here!
+     int a,mul=1 ;
+    scanf("%d",&a);
+    for(int i =1;i<=10;i++){     // 
+         mul=a*i;
+                    printf("%d ",mul);
+
+    }
+    return 0;
+}
