1b0821acf6164ae30edf7f43a165bef6585865cd
pulipatirohithkumar74-wq <pulipatirohithkumar74@gmail.com>
2026-02-12 22:37:06 +0530

    Create countdown program with for loop
    Implement a countdown using a for loop based on user input.

---
+++ Create a countdown using for loop.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+    //Your Code goes here!
+    int i,n;
+    scanf("%d",&n);
+    for(i=n;i>=1;i--){       //i<=1 is an ending point it is telling till where to stop.
+        
+        printf("%d  ",i);
+    }
+    return 0;
+}
