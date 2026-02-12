b1f7796d293badac566f2b4bceeb89ab8ad596af
pulipatirohithkumar74-wq <pulipatirohithkumar74@gmail.com>
2026-02-12 22:35:36 +0530

    Add program to print multiplication table

---
+++ Print multiplication table.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+    //Your Code goes here!
+    int n,i,mul=0;
+    scanf("%d",&n);
+    for(i=1;i<=10;i++){
+        mul=n*i;
+            printf("%d\n",mul);
+
+    }
+    return 0;
+}
