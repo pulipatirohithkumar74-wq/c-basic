e1b2f5ce2589391b38508395218093fb85371bce
pulipatirohithkumar74-wq <pulipatirohithkumar74@gmail.com>
2026-02-20 23:05:25 +0530

    Create function add.c

---
+++ function add.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int add(int a, int b);
+
+int main() {
+    int num1 = 5, num2 = 3;
+    int result = add(num1, num2);   
+    printf("Sum = %d\n", result);
+    return 0;
+}
+
+int add(int a, int b) {
+    return a + b;
+}
