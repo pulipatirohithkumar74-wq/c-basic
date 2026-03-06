91a97c27bde9898386fa7df4280f7a1cba1eb5df
pulipatirohithkumar74-wq <pulipatirohithkumar74@gmail.com>
2026-03-06 22:29:04 +0530

    Create factorial.c

---
+++ factorial.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+    //Your Code goes here!
+    int i,n,fact=1;
+    scanf("%d",&n);
+    for(i=1;i<=n;i++){
+        fact=fact*i;
+    }
+    printf("%d",fact);
+    return 0;
+}
