864761eb71f7854d7e95c067477de62b9cdc9c80
pulipatirohithkumar74-wq <pulipatirohithkumar74@gmail.com>
2026-03-06 22:33:14 +0530

    Create count digits.c

---
+++ count digits.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+    //Your Code goes here!
+    int n,i,count=0;
+    scanf("%d",&n);
+    for(i=1;n!=0;n/=10){
+        count++;
+    }
+    printf("%d",count);
+    return 0;
+}
